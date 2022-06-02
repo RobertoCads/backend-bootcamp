fbc936a1ad38aae9360da2f8153226dbee289d56
Roberto Cadenas <robertocads7@gmail.com>
2022-06-02 12:46:49 +0200

    catch the error before sending

--- README.md
+++ README.md
@@ -9,16 +9,21 @@
 
 
 ## Error Routes
+---
+Didn't find any other status code more specific than 400.
 
-If there are no keys --> Error (405) with message 'The keys can't be empty. The necessary keys are destination and message'
+- If there are no keys --> Error (400) with message 'The keys can't be empty. The necessary keys are destination and message'
 
+- If 'destination' key doesn't exist --> Error (400) with message 'The key destination must exist. The necessary keys are destination and message'
+
+- If 'message' key doesn't exist --> Error (400) with message 'The key message must exist. The necessary keys are destination and message'
+
+
+## Bussiness decisions
 ---
 
-If 'destination' key doesn't exist --> Error (406) with message 'The key destination must exist. The necessary keys are destination and message'
+First I test if there is any error before sending it, and if there are any I show what kind of error with a short explanation. In case it didn't work, I catch the error after is send, and show what kind of error with a short explanation.
 
----
-
-If 'message' key doesn't exist --> Error (407) with message 'The key message must exist. The necessary keys are destination and message'
 
 [POSTMAN TESTS](Tests-Exercise02.postman_collection.json)
 <!-- | .env  | 
--- routes/message.routes.js
+++ routes/message.routes.js
@@ -10,6 +10,30 @@ router.post("/messages", (req, res) => {
 
   const body = message;
 
+  if(!destination && !body) {
+    return res
+      .status(400)
+      .json({
+        message:
+          "The keys can't be empty. The necessary keys are destination and message",
+      });
+  }
+  if(!destination) {
+    return res.status(400).json({
+      message:
+        "The key destination must exist. The necessary keys are destination and message",
+    });
+  }
+  if(!body) {
+    return res
+      .status(400)
+      .json({
+        message:
+          "The key message must exist. The necessary keys are destination and message",
+      });
+  }
+
+
   MessageAppService.sendMessage(destination, body)
     .then((response) => res.json(response.data))
     .catch((err) => {
@@ -17,19 +41,19 @@ router.post("/messages", (req, res) => {
         !err.config.data.includes("destination") &&
         !err.config.data.includes("body")
       ) {
-        return res.status(405).json({
+        return res.status(400).json({
           message:
             "The keys can't be empty. The necessary keys are destination and message",
         });
       }
       if (!err.config.data.includes("destination")) {
-        return res.status(406).json({
+        return res.status(400).json({
           message:
             "The key destination must exist. The necessary keys are destination and message",
         });
       }
       if (!err.config.data.includes("body")) {
-        return res.status(407).json({
+        return res.status(400).json({
           message:
             "The key message must exist. The necessary keys are destination and message",
         });
