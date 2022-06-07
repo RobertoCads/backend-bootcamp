cf79495829475be1e3532c5e6b4dd9b59a71852c
Roberto Cadenas <robertocads7@gmail.com>
2022-06-07 19:42:36 +0200

    added sync.indexes to the models

--- src/models/budget.js
+++ src/models/budget.js
@@ -3,14 +3,22 @@ import mongoose from "mongoose";
 import database from "../database.js";
 import backupDatabase from "../backupDatabase.js";
 
-const budgetSchema = new mongoose.Schema({
-  amount: {
-    type: Number,
-    default: 0,
+const budgetSchema = new mongoose.Schema(
+  {
+    amount: {
+      type: Number,
+      default: 0,
+    },
   },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const Budget = database.model("Budget", budgetSchema);
 const BackupBudget = backupDatabase.model("BackupBudget", budgetSchema);
+
+Budget.syncIndexes();
+BackupBudget.syncIndexes();
 
 export { Budget, BackupBudget };
--- src/models/message.js
+++ src/models/message.js
@@ -3,16 +3,24 @@ import mongoose from "mongoose";
 import database from "../database.js";
 import backupDatabase from "../backupDatabase.js";
 
-const messageSchema = new mongoose.Schema({
-  destination: String,
-  body: String,
-  status: {
-    type: String,
-    enum: ["ERROR", "OK", "TIMEOUT"],
+const messageSchema = new mongoose.Schema(
+  {
+    destination: String,
+    body: String,
+    status: {
+      type: String,
+      enum: ["ERROR", "OK", "TIMEOUT"],
+    },
   },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const Message = database.model("Message", messageSchema);
 const BackupMessage = backupDatabase.model("BackupMessage", messageSchema);
+
+Message.syncIndexes();
+BackupMessage.syncIndexes();
 
 export { Message, BackupMessage };
